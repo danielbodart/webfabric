c772fff5bc6d0e24b7821968b38148ab8d749d58
daniel.bodart <daniel.bodart@localhost>
2009-07-21 15:31:59 +0000

    Minor clean up

--- src/org/webfabric/web/sitemesh/SiteMeshServlet.scala
+++ src/org/webfabric/web/sitemesh/SiteMeshServlet.scala
@@ -1,11 +1,12 @@
 package org.webfabric.web.sitemesh
 
-
+import antlr.stringtemplate.StringTemplate
 import com.opensymphony.module.sitemesh.HTMLPage
 import io.{Url, HierarchicalPath}
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 import servlet.{QueryString, ContextPath}
-import stringtemplate.{PageMap, UrlStringTemplateGroup, PageLoader, UrlPageLoader}
+import stringtemplate.{PageMap, UrlStringTemplateGroup, UrlPageLoader}
+
 class SiteMeshServlet extends HttpServlet{
   override def doGet(request: HttpServletRequest, response: HttpServletResponse) = {
     val content = request.getParameter("content")
@@ -13,18 +14,27 @@ class SiteMeshServlet extends HttpServlet{
     new UrlPageLoader().load(content) match {
       case page: HTMLPage => {
         val decoratorUrl = new Url(request.getParameter("decorator"))
-        val base: Url = decoratorUrl.parent
-        val templateGroup: UrlStringTemplateGroup = new UrlStringTemplateGroup("decorator", base)
-        val StringTemplateDecoratorServlet.TemplateName(name) = decoratorUrl.path.file
-        val template = templateGroup.getInstanceOf(name)
+        val template: StringTemplate = getTemplate(decoratorUrl)
         val decorator = new StringTemplateDecorator(template)
-        decorator.setBase(new ContextPath(base.toString))
+
+        decorator.setBase(new ContextPath(decoratorUrl.parent.toString))
         decorator.setInclude(new PageMap(new UrlPageLoader))
         decorator.setPage(page)
         decorator.setQueryString(QueryString(request))
-        decorator.writeTo(response.getWriter())
+
+        decorator.writeTo(response.getWriter)
       }
-      case _ =>
+      case _ => {
+        response.getWriter.write("Unable to load content: " + content )
+      }
     }
   }
+
+  def getTemplate(url: Url): StringTemplate = {
+    val base = url.parent
+    val templates = new UrlStringTemplateGroup("decorator", base)
+    val StringTemplateDecoratorServlet.TemplateName(name) = url.path.file
+    templates.getInstanceOf(name)
+  }
+
 }
