dd9c4535ea8ea4b3c6ec8a8783afc5a61adee8b0
daniel.bodart <daniel.bodart@localhost>
2009-07-23 16:51:44 +0000

    Added support for targeting divs in client side includes

--- web/js/ahah.js
+++ web/js/ahah.js
@@ -1,5 +1,10 @@
 function Include(element){
-    jQuery.get(element.href, {'decorator': 'body'}, function(data) {
+    var params = {'decorator': 'body'};
+    if( element.href.contains("#") ) {
+        var id = element.href.split("#")[1];
+        params = { 'decorator': 'div', 'id': id }
+    }
+    jQuery.get(element.href, params, function(data) {
         jQuery(element).replaceWith(data);
     });
 }
