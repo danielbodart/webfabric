519e4731d50a85d6f8ad756047dac0dfbd0d2507
dan <dan@laptop>
2009-08-09 11:02:58 +0100

    Added caching tests

--- web/test/caching.tests.js
+++ web/test/caching.tests.js
@@ -8,12 +8,12 @@ test("Sets Cache-Control max-age and Expires to 1 minute and on cacheable conten
       url: "/test/abc.txt",
       complete: function(req, textStatus){
           var expectedSeconds = 60;
-          equals(req.getResponseHeader('Cache-Control'), 'public, max-age=' + expectedSeconds);
+          equals(req.getResponseHeader('Cache-Control'), 'public, max-age=' + expectedSeconds, "Cache-Control");
           var date = new Date(req.getResponseHeader('Date'));
           ok(!isNaN(date), "Date: " + date);
           var expires = new Date(req.getResponseHeader('Expires'));
           ok(!isNaN(expires), "Expires: " + expires);
-          equals(expires - date, expectedSeconds * 1000, "Expected milliseconds");
+          equals(expires - date, expectedSeconds * 1000, "Expires - Date (in milliseconds)");
           start();
       }
     });
