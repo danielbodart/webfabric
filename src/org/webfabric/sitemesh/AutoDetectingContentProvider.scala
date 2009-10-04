e88153b539176eadf746c40c7ccdfe9942787f7d
dan <dan@bodar.com>
2009-10-04 22:44:55 +0100

    Added support to try sitemesh 3 then sitemesh 2

--- src/org/webfabric/sitemesh/AutoDetectingContentProvider.scala
+++ src/org/webfabric/sitemesh/AutoDetectingContentProvider.scala
@@ -1,10 +1,24 @@
 package org.webfabric.sitemesh
 
 import javax.servlet.http.HttpServletRequest
+import org.webfabric.collections.List
+import java.util.Map
 import v2.PagePropertiesProvider
+import v3.ContentPropertiesProvider
 
 class AutoDetectingContentProvider extends ContentProvider{
-  def getContent(request: HttpServletRequest) = {
-    new PagePropertiesProvider().getContent(request)
+  val providers = List(
+    (request: HttpServletRequest) => { new ContentPropertiesProvider().getContent(request) },
+    (request: HttpServletRequest) => { new PagePropertiesProvider().getContent(request) }
+    )
+
+  def getContent(request: HttpServletRequest): Option[Map[_,_]] = {
+    providers.tryPick( provider => {
+        try {
+          provider(request)
+        } catch {
+          case _ => None
+        }
+    })
   }
 }
