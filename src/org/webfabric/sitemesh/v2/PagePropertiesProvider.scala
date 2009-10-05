0ec26de79c116ceb904f92898c9e8676d4cdfb48
dan <dan@mini-laptop>
2009-10-05 12:30:41 +0100

    Created PropertyMap trait

--- src/org/webfabric/sitemesh/v2/PagePropertiesProvider.scala
+++ src/org/webfabric/sitemesh/v2/PagePropertiesProvider.scala
@@ -1,12 +1,11 @@
 package org.webfabric.sitemesh.v2
 
-import org.webfabric.sitemesh.ContentProvider
 import javax.servlet.http.HttpServletRequest
 import com.opensymphony.module.sitemesh.{RequestConstants, HTMLPage}
-import java.util.Map
+import org.webfabric.sitemesh.{PropertyMap, ContentProvider}
 
 class PagePropertiesProvider extends ContentProvider{
-  def getContent(request: HttpServletRequest): Option[Map[_,_]] = {
+  def getContent(request: HttpServletRequest): Option[PropertyMap] = {
     request.getAttribute(RequestConstants.PAGE) match {
       case page: HTMLPage => Some(new PagePropertyMap(page))
       case _ => None
--- src/org/webfabric/sitemesh/v3/ContentPropertiesProvider.scala
+++ src/org/webfabric/sitemesh/v3/ContentPropertiesProvider.scala
@@ -1,13 +1,12 @@
 package org.webfabric.sitemesh.v3
 
-import org.webfabric.sitemesh.ContentProvider
 import javax.servlet.http.HttpServletRequest
 import org.sitemesh.webapp.WebAppContext
 import org.sitemesh.content.Content
-import java.util.Map
+import org.webfabric.sitemesh.{PropertyMap, ContentProvider}
 
 class ContentPropertiesProvider extends ContentProvider{
-  def getContent(request: HttpServletRequest): Option[Map[_,_]] = {
+  def getContent(request: HttpServletRequest): Option[PropertyMap] = {
     request.getAttribute(WebAppContext.CONTENT_KEY) match {
       case content: Content => Some(new ContentPropertyMap(content.getExtractedProperties))
       case _ => None
