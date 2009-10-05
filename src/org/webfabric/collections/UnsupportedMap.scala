7b29dbfdd5f61e35d5a5d6be3285f2758e8f1440
dan <dan@mini-laptop>
2009-10-05 14:17:41 +0100

    Made more specific

--- src/org/webfabric/collections/UnsupportedMap.scala
+++ src/org/webfabric/collections/UnsupportedMap.scala
@@ -1,29 +1,29 @@
 package org.webfabric.collections
 
-import java.util.{Map, Collection}
+import java.util.{Map, Collection, Set}
 
 class UnsupportedMap[Key, Value] extends Map[Key, Value] {
   def values:Collection[Value] = throw new UnsupportedOperationException
 
-  def size = throw new UnsupportedOperationException
+  def size:Int = throw new UnsupportedOperationException
 
-  def remove(key: Any) = throw new UnsupportedOperationException
+  def remove(key: Any):Value = throw new UnsupportedOperationException
 
-  def putAll(m: Map[_ <: Key, _ <: Value]) = throw new UnsupportedOperationException
+  def putAll(m: Map[_ <: Key, _ <: Value]):Unit = throw new UnsupportedOperationException
 
-  def put(key: Key, value: Value) = throw new UnsupportedOperationException
+  def put(key: Key, value: Value):Value = throw new UnsupportedOperationException
 
-  def keySet = throw new UnsupportedOperationException
+  def keySet:Set[Key] = throw new UnsupportedOperationException
 
-  def isEmpty = throw new UnsupportedOperationException
+  def isEmpty:Boolean = throw new UnsupportedOperationException
 
   def get(key: Any):Value = throw new UnsupportedOperationException
 
-  def entrySet = throw new UnsupportedOperationException
+  def entrySet: Set[Map.Entry[Key, Value]] = throw new UnsupportedOperationException
 
   def containsValue(value: Any):Boolean = throw new UnsupportedOperationException
 
   def containsKey(key: Any):Boolean = throw new UnsupportedOperationException
 
-  def clear = throw new UnsupportedOperationException
+  def clear:Unit = throw new UnsupportedOperationException
 }
