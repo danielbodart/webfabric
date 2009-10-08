7a288b2103e95233dd293bdfad270783d29ee56d
dan <dan@mini-laptop>
2009-10-08 20:34:34 +0100

    Converted to use AND and OR expressions

--- src/org/webfabric/collections/FlatMapIterator.scala
+++ src/org/webfabric/collections/FlatMapIterator.scala
@@ -1,26 +1,20 @@
 package org.webfabric.collections
 
-class FlatMapIterator[T,S](iterator:java.util.Iterator[T], handler:(T) => java.lang.Iterable[S]) extends java.util.Iterator[S]{
-  var currentIterator:Option[java.util.Iterator[S]] = None
+class FlatMapIterator[T, S](iterator: java.util.Iterator[T], handler: (T) => java.lang.Iterable[S]) extends java.util.Iterator[S] {
+  var currentIterator: Option[java.util.Iterator[S]] = None
 
-  def hasNext:Boolean = currentIterator match {
-    case None => iterator.hasNext match {
-      case true => {
-        currentIterator = Some(handler(iterator.next).iterator)
-        hasNext
-      }
-      case false => false
+  final def hasNext: Boolean = currentIterator match {
+    case None => iterator.hasNext && {
+      currentIterator = Some(handler(iterator.next).iterator)
+      hasNext
     }
-    case _ => currentIterator.get.hasNext match {
-      case true => true
-      case false => {
-        currentIterator = None
-        hasNext
-      }
+    case Some(it) => it.hasNext || {
+      currentIterator = None
+      hasNext
     }
   }
 
-  def remove = iterator.remove
+  def remove = currentIterator.get.remove
 
   def next = hasNext match {
     case true => currentIterator.get.next
