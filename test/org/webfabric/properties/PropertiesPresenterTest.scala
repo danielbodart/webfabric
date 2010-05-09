054c49dbce36a39c2b770e77896df4e97841c2ab
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-09 17:40:20 +0100

    Made ignore timestamp in header

--- test/org/webfabric/properties/PropertiesPresenterTest.scala
+++ test/org/webfabric/properties/PropertiesPresenterTest.scala
@@ -4,17 +4,15 @@ import org.hamcrest.CoreMatchers._
 import org.junit.Assert._
 import org.junit._
 import java.util.{Properties, Map, HashMap}
-import java.io.{Writer, StringWriter}
+import java.io.{StringReader, Writer, StringWriter}
 
 class PropertiesPresenterTest extends LocalDatastore {
-
   @Test
   def get {
     // setup
     val repository = new PropertiesRepository(datastoreService)
     val properties = new Properties
     properties.setProperty("foo", "bar")
-    val expected = toString(properties)
     var uuid = repository.set(null, properties)
 
     val presenter = new PropertiesPresenter(repository)
@@ -23,22 +21,24 @@ class PropertiesPresenterTest extends LocalDatastore {
     var result = present(presenter, map("uuid" -> uuid))
 
     // verify
-    assertThat(result, is(expected))
+    var actual = new Properties()
+    actual.load(new StringReader(result))
+    assertThat(actual, is(properties))
   }
 
-  def present(presenter: Presenter[Map[String, Object], Writer], map:Map[String, Object]): String = {
+  def present(presenter: Presenter[Map[String, Object], Writer], map: Map[String, Object]): String = {
     val writer = new StringWriter
     presenter.present(map, writer)
     writer.toString
   }
 
-  def toString(properties:Properties):String ={
+  def toString(properties: Properties): String = {
     val writer = new StringWriter
     properties.store(writer, null)
     writer.toString
   }
 
-  def map(pairs:(String, Object)*): Map[String, Object] ={
+  def map(pairs: (String, Object)*): Map[String, Object] = {
     val result = new HashMap[String, Object]
     pairs.foreach(pair => result.put(pair._1, pair._2))
     result
