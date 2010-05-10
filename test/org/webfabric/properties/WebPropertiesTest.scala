d2cf3ce9f193936681070433de5bc876436d2540
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-10 14:31:38 +0100

    Made use random UUID

--- test/org/webfabric/properties/WebPropertiesTest.scala
+++ test/org/webfabric/properties/WebPropertiesTest.scala
@@ -4,19 +4,27 @@ import org.hamcrest.CoreMatchers._
 import org.junit.Assert._
 import org.junit._
 import org.webfabric.io.Url
+import java.util.UUID
 
 class WebPropertiesTest {
+  def url: Url = Url("http://www.webfabric.org/properties/?uuid=" + UUID.randomUUID.toString)
+
   @Test
   def canGetProperties {
-    val properties = new WebProperties(Url("http://www.webfabric.org/properties/?uuid=550e8400-e29b-41d4-a716-446655440000"))
+    val properties = new WebProperties(url)
     assertThat(properties.entrySet.size, is(0))
   }
 
   @Test
   def canPutProperties {
-    val properties = new WebProperties(Url("http://www.webfabric.org/properties/?uuid=550e8400-e29b-41d4-a716-446655440000"))
+    val sameUrl: Url = url
+    var properties = new WebProperties(sameUrl)
     assertThat(properties.entrySet.size, is(0))
     properties.setProperty("foo", "bar")
     properties.flush
+
+    properties = new WebProperties(sameUrl)
+    assertThat(properties.entrySet.size, is(1))
+
   }
 }
