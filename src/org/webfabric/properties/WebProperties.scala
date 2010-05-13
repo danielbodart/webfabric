c0c485ee23d99e5b29f5a54e9c4d387b25b892a2
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-13 17:36:55 +0100

    Fixed to send correct mime type

--- src/org/webfabric/properties/WebProperties.scala
+++ src/org/webfabric/properties/WebProperties.scala
@@ -2,11 +2,13 @@ package org.webfabric.properties
 
 import java.util.Properties
 import org.webfabric.io.Url
+import java.lang.String
 
 class WebProperties(url: Url) extends Properties {
-  this.load(url.inputStream)
+  val mimeType: String = "text/plain"
+  url.get(mimeType, load(_))
 
-  def flush = url.put("text/plain", store(_, null)) match {
+  def flush = url.put(mimeType, store(_, null)) match {
     case (200, _) =>
     case (201, _) =>
     case (204, _) =>
