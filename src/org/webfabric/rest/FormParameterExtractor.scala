924c3e341360ba6d5eb6a0a3aeeb75ebe6c19e2c
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-18 20:41:32 +0100

    Removed unneeded classes

--- src/org/webfabric/rest/FormParameterExtractor.scala
+++
@@ -1,3 +0,0 @@
-package org.webfabric.rest
-
-class FormParameterExtractor(name:String) extends ParameterExtractor(name)
--- src/org/webfabric/rest/PathParameterExtractor.scala
+++
@@ -1,3 +0,0 @@
-package org.webfabric.rest
-
-class PathParameterExtractor(name:String) extends ParameterExtractor(name)
