200f3a8e39ac03fcd7fc60c2a57ff386f3e7c236
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-20 13:43:34 +0100

    More clean up on

--- src/org/webfabric/rest/HttpMethodActivator.scala
+++ src/org/webfabric/rest/HttpMethodActivator.scala
@@ -1,21 +1,16 @@
 package org.webfabric.rest
 
 import java.lang.reflect.Method
-import org.webfabric.collections.{List}
 import javax.ws.rs._
 import com.googlecode.yadic.{Resolver}
 
 class HttpMethodActivator(httpMethod: String, resource: Class[_], method: Method) extends Matcher[Request]{
-  lazy val extractors: List[ParameterExtractor] = {
-    val result = List[ParameterExtractor]()
-    method.getParameterAnnotations.foreach(_(0) match {
-      case query: QueryParam => result.add(new QueryParameterExtractor(query))
-      case form: FormParam => result.add(new FormParameterExtractor(form))
-      case path: PathParam => result.add(new PathParameterExtractor(path, pathTemplate))
-      case _ =>
-    })
-    result
-  }
+  lazy val extractors = method.getParameterAnnotations.map(_(0) match {
+      case query: QueryParam => new QueryParameterExtractor(query)
+      case form: FormParam => new FormParameterExtractor(form)
+      case path: PathParam => new PathParameterExtractor(path, pathTemplate)
+      case _ => null
+    }).filter(_ != null)
 
   lazy val pathTemplate: UriTemplate = {
     val paths = List(resource.getAnnotation(classOf[Path]), method.getAnnotation(classOf[Path]))
@@ -29,18 +24,12 @@ class HttpMethodActivator(httpMethod: String, resource: Class[_], method: Method
             mimeMatcher.isMatch(request) && parametersMatch(request)
   }
   
-  def parametersMatch(request:Request): Boolean = {
-    extractors.foldLeft(true, (isMatch: Boolean, extractor) => isMatch && extractor.isMatch(request))
-  }
+  def parametersMatch(request:Request): Boolean = extractors.forall(_.isMatch(request))
 
   def activate(container: Resolver, request:Request): Object = {
     val resourceInstance = container.resolve(resource)
     method.invoke(resourceInstance, getParameters(request): _*)
   }
 
-  def getParameters(request:Request): Array[Object] = {
-    val results = List[Object]()
-    extractors.foreach(extractor => results.add(extractor.extract(request)))
-    results.toArray
-  }
+  def getParameters(request:Request): Array[Object] = extractors.map(extractor => extractor.extract(request))
 }
