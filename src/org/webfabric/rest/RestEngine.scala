32926fbc5f5446581100f6834496be27dada446c
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-23 09:48:37 +0100

    Seperated container from engine, so that resource life cycle is determained in usual manner

--- src/org/webfabric/rest/RestEngine.scala
+++ src/org/webfabric/rest/RestEngine.scala
@@ -2,19 +2,15 @@ package org.webfabric.rest
 
 import org.webfabric.collections.Iterable.toMyIterable
 import java.lang.reflect.Method
-import com.googlecode.yadic.SimpleContainer
 import org.webfabric.collections.List
 import javax.ws.rs.{HttpMethod}
-import java.lang.{String, Class}
-import javax.ws.rs.core.StreamingOutput
-import java.io.{OutputStreamWriter, ByteArrayOutputStream, OutputStream}
+import java.lang.{Class}
+import com.googlecode.yadic.{Resolver}
 
 class RestEngine {
   val activators = List[HttpMethodActivator]()
-  val container = new SimpleContainer
 
   def add(resource: Class[_]): Unit = {
-    container.add(resource)
     resource.getMethods.foreach(method => getHttpMethod(method) match {
       case Some(httpMethod) => activators.add(new HttpMethodActivator(httpMethod.value, resource, method))
       case _ =>
@@ -28,33 +24,9 @@ class RestEngine {
     }))
   }
 
-  def handle(request:Request): String = {
-    activate(request) match {
-      case value: String => value
-      case streaming: StreamingOutput => {
-        val out = new ByteArrayOutputStream
-        streaming.write(out)
-        out.toString
-      }
-      case null => null
-    }
-  }
-
-  def handle(request:Request, output: OutputStream): Unit = {
-    activate(request) match {
-      case value: String => {
-        var streamWriter = new OutputStreamWriter(output)
-        streamWriter.write(value)
-        streamWriter.flush
-      }
-      case streaming: StreamingOutput => streaming.write(output)
-      case null => null
-    }
-  }
-
-  def activate(request:Request): Object = {
+  def handle(container:Resolver, request:Request, response:Response): Unit = {
     findActivator(request) match {
-      case Some(activator) => activator.activate(container, request)
+      case Some(activator) => activator.activate(container, request, response)
       case _ => error("No match found")
     }
   }
