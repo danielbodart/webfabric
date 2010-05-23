32b25fa8397e13944aab11224dc71aef0d7c29ca
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-23 09:05:49 +0100

    Added inital module concept - unused

---
+++ src/org/webfabric/properties/PropertiesModule.scala
@@ -0,0 +1,31 @@
+package org.webfabric.properties
+
+import com.googlecode.yadic.Container
+import com.google.appengine.api.datastore.{DatastoreService, DatastoreServiceFactory}
+import org.antlr.stringtemplate.StringTemplateGroup
+import org.webfabric.stringtemplate.UrlStringTemplateGroup
+import org.webfabric.io.RelativeResource
+import org.webfabric.rest.{RestEngine, Module, Request}
+
+class PropertiesModule extends Module{
+  def addPerRequestObjects(container: Container) = {
+    container.add(classOf[PropertiesResource])
+    container.add(classOf[PropertiesRepository])
+    container.add(classOf[DatastoreService], () => DatastoreServiceFactory.getDatastoreService)
+    container.add(classOf[StringTemplateGroup], () => new UrlStringTemplateGroup("templates", baseUrl))
+    this
+  }
+
+  def addPerApplicationObjects(container: Container) = this
+
+
+  def addResource(engine: RestEngine) = {
+    engine.add(classOf[PropertiesResource])
+    this
+  }
+
+  def baseUrl = {
+    val url = RelativeResource.asUrl(classOf[PropertiesApplication], "editor.st")
+    url.replacePath(url.path.parent)
+  }
+}
