3d6405b8856402f7d064cc7783dbfdb14448678f
Daniel Worthington-Bodart <dan@bodar.com>
2010-06-02 08:24:39 +0100

    Added support to type coercion

--- src/org/webfabric/rest/Extractors.scala
+++ src/org/webfabric/rest/Extractors.scala
@@ -1,6 +1,5 @@
 package org.webfabric.rest
 
-import coercers.Coercers
 import javax.ws.rs.{QueryParam, FormParam, PathParam}
 import java.io.InputStream
 import java.lang.reflect.Method
@@ -15,15 +14,5 @@ object Extractors {
         case _ => null
       }
     }).filter(_ != null)
-  }
-
-  class CoercionExtractor(parameterType:Class[_], extractor:RequestExtractor[Object]) extends RequestExtractor[Object]{
-    val coercer = new Coercers()
-    def isMatch(request: Request) = extractor.isMatch(request)
-
-    def extract(request: Request):Object = {
-      val value = extractor.extract(request)
-      coercer.coerce(value, parameterType)
-    }
   }
 }
--- src/org/webfabric/rest/coercers/PassThroughCoercer.scala
+++
@@ -1,9 +0,0 @@
-package org.webfabric.rest.coercers
-
-import java.lang.Class
-
-class PassThroughCoercer extends TypeCoercer {
-  def canCoerce(value: Object, result: Class[_]) = result.isAssignableFrom(value.getClass)
-
-  def coerce(value: Object, result: Class[_]): Object = value
-}
