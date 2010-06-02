77ad9d50a77a34aed170880ffaa4aba73ef52dd9
Daniel Worthington-Bodart <dan@bodar.com>
2010-06-02 20:22:02 +0100

    Removed extractors

--- src/org/webfabric/rest/InputStreamExtractor.scala
+++
@@ -1,9 +0,0 @@
-package org.webfabric.rest
-
-import java.io.InputStream
-
-class InputStreamExtractor extends RequestExtractor[InputStream]{
-  def isMatch(request: Request) = true
-
-  def extract(request: Request) = request.input
-}
