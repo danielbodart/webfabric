aa5cbc1b272cf2c0866c4d22f811475eb7c7355d
Daniel Worthington-Bodart <dan@bodar.com>
2010-06-02 20:18:29 +0100

    Moved more behavour in to arguments extractor

--- src/org/webfabric/rest/ArgumentsExtractor.scala
+++ src/org/webfabric/rest/ArgumentsExtractor.scala
@@ -1,11 +1,11 @@
 package org.webfabric.rest
 
-import javax.ws.rs.{QueryParam, FormParam, PathParam}
 import com.googlecode.yadic.{SimpleContainer, Container}
-import java.io.InputStream
 import java.lang.reflect.Method
+import java.io.{ByteArrayInputStream, InputStream}
+import javax.ws.rs.{HeaderParam, QueryParam, FormParam, PathParam}
 
-class ArgumentsExtractor(pathExtractor:PathExtractor,resource: Class[_], method: Method) extends RequestExtractor[Array[Object]]{
+class ArgumentsExtractor(pathExtractor: PathExtractor, resource: Class[_], method: Method) extends RequestExtractor[Array[Object]] {
   def isMatch(request: Request) = {
     try {
       extract(request)
@@ -15,7 +15,7 @@ class ArgumentsExtractor(pathExtractor:PathExtractor,resource: Class[_], method:
     }
   }
 
-  def extract(request: Request):Array[Object] = {
+  def extract(request: Request): Array[Object] = {
     val container = getArgumentContainer
     container.addInstance(request)
     method.getParameterTypes.zip(method.getParameterAnnotations).map(pair => {
@@ -26,18 +26,23 @@ class ArgumentsExtractor(pathExtractor:PathExtractor,resource: Class[_], method:
       if (annotations.length > 0) annotations(0) match {
         case query: QueryParam => container.add(classOf[String], () => {
           val params = container.resolveType(classOf[QueryParameters])
-          if(!params.contains(query.value)) throw new NoSuchElementException
+          if (!params.contains(query.value)) throw new NoSuchElementException
           params.getValue(query.value)
         })
         case form: FormParam => container.add(classOf[String], () => {
           val params = container.resolveType(classOf[FormParameters])
-          if(!params.contains(form.value)) throw new NoSuchElementException
+          if (!params.contains(form.value)) throw new NoSuchElementException
           params.getValue(form.value)
         })
         case path: PathParam => container.add(classOf[String], () => {
           val params = container.resolveType(classOf[PathParameters])
-          if(!params.contains(path.value)) throw new NoSuchElementException
+          if (!params.contains(path.value)) throw new NoSuchElementException
           params.getValue(path.value)
+        })
+        case header: HeaderParam => container.add(classOf[String], () => {
+          val params = container.resolveType(classOf[HeaderParameters])
+          if (!params.contains(header.value)) throw new NoSuchElementException
+          params.getValue(header.value)
         })
         case _ =>
       }
@@ -54,5 +59,27 @@ class ArgumentsExtractor(pathExtractor:PathExtractor,resource: Class[_], method:
     container.add(classOf[FormParameters], () => container.resolveType(classOf[Request]).form)
     container.add(classOf[InputStream], () => container.resolveType(classOf[Request]).input)
     container
+  }
+
+  def generate(arguments: Array[Object]): Request = {
+    val pathParams = PathParameters()
+    val headers = HeaderParameters()
+    val formParams = FormParameters()
+    val queryParams = QueryParameters()
+    var input: InputStream = new ByteArrayInputStream(new Array[Byte](0))
+
+    arguments.zip(method.getParameterAnnotations).map(pair => {
+      val value = pair._1
+      val annotations = pair._2
+
+      if (annotations.length > 0) annotations(0) match {
+        case path: PathParam => pathParams.add(path.value, value.toString)
+        case form: FormParam => formParams.add(form.value, value.toString)
+        case query: QueryParam => queryParams.add(query.value, value.toString)
+        case header: HeaderParam => headers.add(header.value, value.toString)
+        case _ =>
+      }
+    })
+    Request(null, pathExtractor.pathTemplate.generate(pathParams), headers, queryParams, formParams, input)
   }
 }
