bdbf2fba2c97f7ee0fe80a74e5216e72aba7cc1a
Daniel Wothington-Bodart <dan@bodar.com>
2010-07-05 14:22:45 +0100

    Finished making the Rest Application generic

--- src/org/webfabric/rest/ApplicationStarter.scala
+++ src/org/webfabric/rest/ApplicationStarter.scala
@@ -3,10 +3,20 @@ package org.webfabric.rest
 import org.webfabric.servlet.{ContextPath, BasePath, WarRoot}
 import javax.servlet.{ServletContext, ServletContextEvent, ServletContextListener}
 import org.webfabric.properties.PropertiesApplication
+import com.googlecode.yadic.SimpleContainer
 
 class ApplicationStarter extends ServletContextListener{
   def createApplication(servletContext: ServletContext): Application = {
-    new PropertiesApplication
+    val className = servletContext.getInitParameter(getClass.getName)
+    if(className == null || className.equals("")){
+      throw new UnsupportedOperationException("The web.xml must container a init-param for :" + getClass.getName + " ")
+    }
+    val aClass = Class.forName(className)
+    val instance = new SimpleContainer().add(aClass).resolve(aClass)
+    if(!instance.isInstanceOf[Application]){
+      throw new UnsupportedOperationException(className + " must implement " + classOf[Application].getName )
+    }
+    instance.asInstanceOf[Application]
   }
 
   def contextInitialized(event: ServletContextEvent) = {
